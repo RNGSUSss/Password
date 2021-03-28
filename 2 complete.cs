9b893f453452fd3a0ed69a53ef85a8fa1d4f74af
RNGSUSss <78634323+RNGSUSss@users.noreply.github.com>
2021-03-28 15:40:41 +0700

    Add files via upload

---
+++ 2 complete.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace sobeklaew
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, x, y;
+            Console.Write("input number a : ");
+            a = int.Parse(Console.ReadLine());
+            Console.Write("input number b : ");
+            b = int.Parse(Console.ReadLine());
+            x = a;
+            y = b;
+            do
+            {
+                if (x < y) { x = x + a; }
+                else { y = y + b; }
+
+            }
+            while (x != y);
+            Console.Write("X is {0}", x);
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
