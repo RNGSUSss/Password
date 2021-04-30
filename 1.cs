d64f53ac1ce725c08aafb6e9c95dd288e7d2d0d6
RNGSUSss <78634323+RNGSUSss@users.noreply.github.com>
2021-04-30 22:43:29 +0700

    Add files via upload

---
+++ 1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace work1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double num = 1;
+            Console.Write("in put your number : ");
+            double number = double.Parse(Console.ReadLine());
+            if (number < 0) { Console.Write("Invalid Pascal's triangle row number"); }
+
+            for (int i = 0; i <= number; i++) //line
+            {
+                for (int j = 0; j <= i; j++) //row
+                {
+                    if (i == 0 || j == 0) { num = 1; } //number
+                    else num = num * (i - j + 1) / j;
+                    Console.Write("{0} ",num);
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+    }
+}
