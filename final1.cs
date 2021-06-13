1a82f5bd449fdfb55bff58721bcec54f366ad34b
RNGSUSss <78634323+RNGSUSss@users.noreply.github.com>
2021-06-13 15:27:19 +0700

    Add files via upload

---
+++ final1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace final_test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int width = 3;
+            int height = 3;
+            int[,] block = new int[width,height];
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    block[i,j] = int.Parse(Console.ReadLine());
+                }
+            }
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    Console.Write(block[i,j]);
+                }
+                Console.Write("\n");
+            }
+            Console.ReadLine();
+        }
+    }
+}
